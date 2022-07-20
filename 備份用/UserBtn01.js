5dcd8e30020dbf3e40c5a06134d8a8af56ffc400
Lemon-2001 <b10802084@g.chu.edu.tw>
2022-07-20 20:42:21 +0800

    新輩分

--- 備份用/UserBtn01.js
+++ 備份用/UserBtn01.js
@@ -1,10 +1,10 @@
 
 //編輯畫面1 關閉編輯以及取消編輯以及儲存編輯
 function ChangeDisabled(value) {
-    var EditInput05 = document.getElementById('EditInput01');
-    var EditInput05 = document.getElementById('EditInput02');
-    var EditInput05 = document.getElementById('EditInput03');
-    var EditInput05 = document.getElementById('EditInput04');
+    var EditInput01 = document.getElementById('EditInput01');
+    var EditInput02 = document.getElementById('EditInput02');
+    var EditInput03 = document.getElementById('EditInput03');
+    var EditInput04 = document.getElementById('EditInput04');
     var EditInput05 = document.getElementById('EditInput05');
     if (value == '1') {
         EditInput01.disabled = false;　// 變更欄位為可用
@@ -62,11 +62,21 @@ let UserAbout = document.querySelector('.E1_UserAbout')
 
 E1_submitValue()
 function E1_submitValue() {
-
-    //國家/地區
-    country.addEventListener('change', function () {
-        var option = this.options[this.selectedIndex];
-        $(`.E1_country`).attr("value", `${option.value}`)
+    //個人檔案名稱
+    UserName.addEventListener('onchange', function () {
+        var option = this.UserName
+        $(`.E1_UserName`).attr("value", `${option.value}`)
         debugger
     })
+    //密碼
+    //確認密碼
+    //國家/地區
+    country.addEventListener('change', function () {
+        //var option = this.options[this.selectedIndex];
+        //$(`.E1_country`).attr("value", `${option.value}`)
+        var Str = UserName.value
+        alert(Str)
+        debugger
+    })
+    //關於我
 }
